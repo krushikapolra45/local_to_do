29bb5ab7d193919c1d456a1966c33e9e4b48c644
krushika Polra <kpolara548@gmail.com>
2023-09-02 18:42:27 +0530

    Initial commit

---
+++ lib/view/share_pref_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SharePrefScreen extends StatefulWidget {
+  const SharePrefScreen({super.key});
+
+  @override
+  State<SharePrefScreen> createState() => _SharePrefScreenState();
+}
+
+class _SharePrefScreenState extends State<SharePrefScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
